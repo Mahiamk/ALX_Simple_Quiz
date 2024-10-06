2fbd712306a9fca4f6965ca55d7caf6894de7ff9
Anwar Koji <camk0966@gmail.com>
2024-10-07 02:25:16 +0800

    All created

--- quiz.js
+++ quiz.js
@@ -3,11 +3,11 @@ function  checkAnswer(){
   const correctAnswer="4";
   const userAnswer = document.querySelector('input[name="quiz"]:checked').value;
   const feedback=document.getElementById('feedback');
-  if (userAnswer===correctAnswer){
-    feedback.textContent= "Correct! Well done.";
+  if (userAnswer==correctAnswer){
+    feedback.textContent = "Correct! Well done.";
 
   }else{
-    feedback.textContent="That's incorrect. Try again!";
+    feedback.textContent ="That's incorrect. Try again!";
   }
 
 }
