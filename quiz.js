55a7fab58653641649267ca9a41ad9a23846f445
Anwar Koji <camk0966@gmail.com>
2024-10-07 03:05:45 +0800

    All created

--- quiz.js
+++ quiz.js
@@ -1,15 +1,17 @@
 
 function  checkAnswer(){
   const correctAnswer="4";
-  const userAnswer = document.querySelector('input[name="quiz"]:checked').value;
+  const userAnswer = document.querySelector('input[name="quiz"]:checked');
   const feedback=document.getElementById('feedback');
-  if (userAnswer==correctAnswer){
-    feedback.textContent = "Correct! Well done.";
+  if (userAnswer){
+    if (userAnswer.value === correctAnswer){
+      feedback.textContent = "Correct! Well done.";
+    }else{
+      feedback.textContent ="That's incorrect. Try again!";
+    }
 
   }else{
-    feedback.textContent ="That's incorrect. Try again!";
+    feedback.textContent="Thatis is incorrect. Choose one answer at a time!"
   }
-
 }
 document.getElementById('submit-answer').addEventListener('click', checkAnswer);
-
